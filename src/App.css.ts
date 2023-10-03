352bbe0a70665cb1ad9117078aa410a75d764376
Hypekage <jmestmnag@gmail.com>
2023-10-03 23:11:10 +0545

    implementing global ` theme `

--- src/App.css.ts
+++ src/App.css.ts
@@ -1,9 +1,11 @@
 import { style } from "@vanilla-extract/css";
-
+import { global } from "./global.css";
 export const flexContainer = style({
   display: "flex",
   justifyContent: "center",
   height: "100vh",
   alignItems: "center",
-  width:'100vw'
+  width: "100vw",
+  backgroundColor: global.colors.primary,
 });
+
