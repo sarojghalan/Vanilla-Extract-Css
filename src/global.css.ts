5cc1c7fb7ee36fe21aa3bdcc1151fb5b216c4190
Hypekage <jmestmnag@gmail.com>
2023-10-03 23:10:00 +0545

    creating global theme, `createGloabalTheme `

---
+++ src/global.css.ts
@@ -0,0 +1,9 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+
+export const global = createGlobalTheme("#root", {
+  colors: {
+    primary: "#ff0000 ",
+    secondary: "#0000FF",
+  },
+});
+
