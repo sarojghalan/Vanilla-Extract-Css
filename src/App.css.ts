5d58b7d8d116d7f9320b5c396cf6cc45d471b5c0
Hypekage <jmestmnag@gmail.com>
2023-10-03 21:17:01 +0545

    playing around with ` vanilla-extract-css `

---
+++ src/App.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+export const flexContainer = style({
+  display: "flex",
+  justifyContent: "center",
+  height: "100vh",
+  alignItems: "center",
+  width:'100vw'
+});
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { flexContainer } from "./App.css"
+
+const App = () => {
+  return (
+    <div className={flexContainer}>App</div>
+  )
+}
+
+export default App
